9e802b9c3cb5fd106c0c7ea27fec48ed75709097
insertvalue2 <insertvalue@naver.com>
2022-06-30 23:56:26 +0900

    Revert "검색기능 추가 v1"
    This reverts commit e014eb126b5f617f767b00a135087581bbec8158.

--- src/main/java/com/tecoding/blog/api/BoardApiController.java
+++ src/main/java/com/tecoding/blog/api/BoardApiController.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tecoding.blog.auth.PrincipalDetail;
@@ -47,11 +45,11 @@ public class BoardApiController {
 	}
 
 	@PostMapping("/api/board/{boardId}/reply")
-	public ResponseDto<Reply> replySave(@PathVariable int boardId, @RequestBody Reply reply,
-			@AuthenticationPrincipal PrincipalDetail pricipalDetail) {
-
-		// 서비스 데이터 처리
-		Reply replyEntity = boardService.writeReply(pricipalDetail.getUser(), boardId, reply);
+	public ResponseDto<Reply> replySave(@PathVariable int boardId,
+			@RequestBody Reply reply, @AuthenticationPrincipal PrincipalDetail pricipalDetail) {
+		
+		// 서비스 데이터 처리 
+		Reply replyEntity  = boardService.writeReply(pricipalDetail.getUser(), boardId, reply);
 		return new ResponseDto<Reply>(HttpStatus.OK, replyEntity);
 	}
 
@@ -66,8 +64,7 @@ public class BoardApiController {
 	@DeleteMapping("/api/board/{boardId}/reply/{replyId}")
 	public ResponseDto<Integer> deleteReply(@PathVariable int replyId) {
 		boardService.deleteReplyById(replyId);
-		return new ResponseDto<Integer>(HttpStatus.OK, 1);
+		return new ResponseDto<Integer>(HttpStatus.OK, 1); 
 	}
-
 
 }
--- src/main/java/com/tecoding/blog/repository/BoardRepository.java
+++ src/main/java/com/tecoding/blog/repository/BoardRepository.java
@@ -1,12 +1,9 @@
 package com.tecoding.blog.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tecoding.blog.model.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-	// select * from board where title like '%a%';
-	Page<Board> findByTitleContaining(String searchValue, Pageable pageable);
+
 }
